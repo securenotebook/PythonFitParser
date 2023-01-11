0bb826e84afd4c00fb2ae76a2d1561c1713d9599
Wes <wesley.elder@gmail.com>
2023-01-11 06:36:19 +0100

    Auto Generate Link

--- fitparsev3.py
+++ fitparsev3.py
@@ -1,14 +1,20 @@
 from fitparse import FitFile
 from datetime import timedelta
 from influxdb import InfluxDBClient
+import platform
+import pytz
 
-#Change IP to DocketCompose hostName
-client = InfluxDBClient(host='influxdb', port=8086) # Connect to the InfluxDB instance
+#TODO - Influx setup
+client = InfluxDBClient(host='192.168.1.155', port=8086) # Connect to the InfluxDB instance
 client.drop_database('strava')      # Drop Old DB
 client.create_database('strava')    # Create DB
 
 # create list of fitFiles
-files = {"quarq":FitFile('fitFiles/FTP-Quarq.fit'),  "kickr":FitFile('fitFiles/FTP-Kickr.fit'),  "assioma": FitFile('fitFiles/FTP-Assioma.fit')}
+files = {"quarq":FitFile('fitFiles/FTP-Quarq.fit'), "kickr":FitFile('fitFiles/FTP-Kickr.fit'),  "assioma": FitFile('fitFiles/FTP-Assioma.fit')}
+
+grafana_link = ""
+min_timestamp =0
+max_timestamp =0
 
 #Loop through file
 for id in files: 
@@ -23,23 +29,45 @@ for id in files:
         power = record.get_value('power') # Get the power data
 
         if power == None: power=0 # replace none with zero
-
+        
         timestamp = record.get_value('timestamp') # Get the timestamp
+        utc = timestamp  + timedelta(hours=1)
+        
+        epoch = int(utc.timestamp()*1000)
+        
+        # print(epoch)
+        if min_timestamp == 0: min_timestamp = epoch
+        if epoch < min_timestamp : min_timestamp = epoch
+        if epoch > max_timestamp: max_timestamp = epoch
+
+       
+        # record.get_value('timestamp').
         
         #make some minor adjustments to timestamps
-        # if id=="kirkr": timestamp = timestamp  - timedelta(seconds=7)
+        # 
         # if id=="assioma": timestamp = timestamp  - timedelta(seconds=4)
         
         #build the data point to write
-        data = {
-                'measurement': 'strava',
+        data = {'measurement': 'strava',
                 'time': timestamp,
                 'tags': { 'powerMeter': id},
-                'fields': {'power': power}
-            }
+                'fields': {'power': power}}
 
         dataPoints.append(data)
         
     print(f"{id} - Data Loaded - Writing to influx")
     client.write_points(dataPoints, database='strava')
     print(f"{id} Data written")
+
+link = f"http://192.168.1.155:3000/d/fit/fit-power-comparison?orgId=1&from={min_timestamp}&to={max_timestamp}"
+link = f"http://192.168.1.155:3000/d/qJnkG6cVk/new-dashboard?orgId=1&from={min_timestamp}&to={max_timestamp}"
+print(link)
+print(min_timestamp)
+print(max_timestamp)
+
+# Min
+# 1673322014626  
+# 1673318438000
+
+# Max
+# 1673325848883  v 1673322238000
